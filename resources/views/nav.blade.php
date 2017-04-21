026aa681d3c5d979542f0397cf22396d66ac675c
Harry Zhou <zhouziqun@cool2645.com>
2017-04-22 01:17:10 +0800

    Fix teaminfo ref.

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -3,11 +3,11 @@
         <img class="hide-on-med-and-down" src="/img/neu.png"/>
         <a style="margin-left: 20px;" id="logo-container" href="/" class="brand-logo">成语大会初赛在线答题</a>
         <ul class="right hide-on-med-and-down">
-            <li><a href="#">组队信息查询</a></li>
+            <li><a href="/teaminfo">组队信息查询</a></li>
         </ul>
 
         <ul id="nav-mobile" class="side-nav">
-            <li><a href="#">组队信息查询</a></li>
+            <li><a href="/teaminfo">组队信息查询</a></li>
         </ul>
         <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
