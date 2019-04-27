10b47c2c9c97bc2d1d83789141164a282eeaef1b
Bittersweet <zhouziqun@cool2645.com>
2019-04-27 19:03:36 +0800

    :sparkles: add APP_NAME_SHORT

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="light-blue darken-1" role="navigation">
     <div class="nav-wrapper container">
         <img class="hide-on-med-and-down" src="/img/neu.png"/>
-        <a style="margin-left: 20px;" id="logo-container" href="/" class="brand-logo">{{env('APP_NAME')}}</a>
+        <a style="margin-left: 20px;" id="logo-container" href="/" class="brand-logo">{{env('APP_NAME_SHORT')}}</a>
         {{-- PC--}}
         <ul class="right hide-on-med-and-down">
             <li><a href="/teaminfo">组队信息查询</a></li>
